9450e3ea2544d590b5adec1aaafd00eaf6879e15
Ali Erdem Akın <erdemakin09@gmail.com>
2017-02-28 11:37:45 +0300

    Add project files.

---
+++ YazLab/Control.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YazLab
+{
+    public static class Control
+    {
+        public static string filePath { get; set; }
+        public static string fileName { get; set; }
+        public static List<string> neighbor = new List<string>();
+        public static int inputMatrisSize = 0;
+        public static int[,] rMatris = new int[inputMatrisSize, inputMatrisSize];
+        public static int[,] qMatris = new int[inputMatrisSize, inputMatrisSize];
+
+
+        public static void CreateRMatris()
+        {
+            int i = 0;
+            foreach (string value in neighbor)
+            {
+                if (value.Length > 1)
+                {
+                    string[] keys = value.Split(',');
+                    foreach(string key in keys)
+                    {
+                        for (int y = 0; y < inputMatrisSize; y++)
+                        {
+                            if (y.ToString() == key)
+                            {
+                                rMatris[i, y] = 0;
+                            }
+                            else
+                            {
+                                if(rMatris[i,y] != 0)
+                                {
+                                    rMatris[i, y] = -1;
+                                }
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    for (int y = 0; y < inputMatrisSize; y++)
+                    {
+                        if (y.ToString() == value)
+                        {
+                            rMatris[i, y] = 0;
+                        }
+                        else
+                        {
+                            if (rMatris[i, y] != 0)
+                            {
+                                rMatris[i, y] = -1;
+                            }
+                        }
+                    }
+                }
+                i++;
+            }
+        }
+    }
+}
---
+++ YazLab/R-Matris.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace YazLab
+{
+    public partial class R_Matris : Form
+    {
+        public R_Matris()
+        {
+            InitializeComponent();
+        }
+
+        private void R_Matris_Load(object sender, EventArgs e)
+        {
+            Control.CreateRMatris();
+            for(int i=0;i<Control.inputMatrisSize;i++)
+            {
+                for(int y=0;y<Control.inputMatrisSize;y++)
+                {
+                    richTextBox1.Text += Control.rMatris[i, y];
+                }
+                richTextBox1.Text += "\n";
+            }
+        }
+    }
+}
