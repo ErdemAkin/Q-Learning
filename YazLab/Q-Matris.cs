57cf2eb664d0e5f4b6feb70b3cd4c8c4714aadbb
Ali Erdem Akın <erdemakin09@gmail.com>
2017-03-16 15:32:03 +0300

    Proje Btti

--- YazLab/Q-Matris.cs
+++ YazLab/Q-Matris.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,6 +25,25 @@ namespace YazLab
             Control.initQDataTable();
             Control.FindPath();
             dataGridView2.DataSource = Control.qtable;
+
+            String filePath = "C:\\Users\\Ali Erdem\\Desktop\\Output\\qMatris.txt";
+            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            for (int i = 0; i < Control.inputMatrisSize; i++)
+            {
+                String Row = "";
+                for (int y = 0; y < Control.inputMatrisSize; y++)
+                {
+                    Row += "[";
+                    Row += Control.qMatris[i, y];
+                    Row += "]";
+                }
+                sw.WriteLine(Row);
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
         }
     }
 }
--- YazLab/R-Matris.cs
+++ YazLab/R-Matris.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -22,6 +23,25 @@ namespace YazLab
             Control.CreateRMatris();
             Control.initRDataTable();
             dataGridView1.DataSource = Control.rtable;
+
+            String filePath = "C:\\Users\\Ali Erdem\\Desktop\\Output\\rMatris.txt";
+            FileStream fs = new FileStream(filePath, FileMode.OpenOrCreate, FileAccess.Write);
+            StreamWriter sw = new StreamWriter(fs);
+
+            for(int i = 0; i<Control.inputMatrisSize; i++)
+            {
+                String Row = "";
+                for(int y = 0; y<Control.inputMatrisSize; y++)
+                {
+                    Row += "[";
+                    Row += Control.rMatris[i, y];
+                    Row += "]";
+                }
+                sw.WriteLine(Row);
+            }
+            sw.Flush();
+            sw.Close();
+            fs.Close();
         }
     }
 }
